2de212f0602ddba3154a1c78d6a74c7607c3af4b
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2025-10-12 13:14:20 -0500

    started

---
+++ Password_Gen/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".option input");
+const copyIcon = document.querySelector("input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+};
+
+// generate password
+
+const generatePassword = () => {
+  let staticPassword = "";
+  randomPassword = "";
+  excludeDuplicate = false;
+  passLength = lengthSlider.value;
+
+  options.forEach(option => {
+    if (option.checked) {
+
+    }
+  });
+
+}
+
